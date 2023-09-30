fa456cc95a92d4892f27869c530a632815a4fa3e
ravindra135 <reddyravindra947@gmail.com>
2023-09-30 16:19:31 +0530

    Json & Postman Response

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,3 +1,17 @@
 exports.getPosts = (req, res) => {
-    res.send("Hello World from Node JS!");
+    // res.send("Hello World from Node JS!");
+    res.json({
+        posts: [
+            {
+                title: "Node JS First Post",
+                author: "R Cube Dev",
+                posted_on: "2023-09-30 10:28:15"
+            },
+            {
+                title: "Node JS Second Post",
+                author: "Kanhu",
+                posted_on: "2023-09-26 02:04:45"
+            }
+        ]
+    });
 }
