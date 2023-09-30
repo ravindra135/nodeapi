c9bb101e705081bfaaed3bb2eb47d3ef43013665
ravindra135 <reddyravindra947@gmail.com>
2023-09-30 13:45:33 +0530

    created seperated routes file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello World from Node JS");
-});
+// app.get("/", function(req, res) {
+//     res.send("Hello World from Node JS");
+// });
+
+const { getPosts } = require('./routes/post');
+
+app.get('/', getPosts);
 
 // app.listen(8080);
 const port = 8080;
