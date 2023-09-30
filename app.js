a239e27fe820aedb8d4134cb680c7d87fcfacbeb
ravindra135 <reddyravindra947@gmail.com>
2023-09-30 13:59:31 +0530

    Working with Middleware

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 // app.get("/", function(req, res) {
 //     res.send("Hello World from Node JS");
 // });
 
+// Routes Seperated;
 const { getPosts } = require('./routes/post');
 
+// Demo: Creating custom middleware
+// const customMiddleware = (req, res, next) => {
+//     console.log("middleware applied");
+//     next();
+// };
+
+// Middleware using Morgan;
+app.use(morgan("dev"));
+
+// Demo
+// app.use(customMiddleware);
+
 app.get('/', getPosts);
+
+//-> This way you can protect your individual route with middleware
+// app.get('/', customMiddleware, getPosts);
+
+
 
 // app.listen(8080);
 const port = 8080;
