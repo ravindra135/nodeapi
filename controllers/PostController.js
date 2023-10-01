252520a56490b8bce3b0552efc6786a9360066c7
ravindra135 <reddyravindra947@gmail.com>
2023-10-01 10:21:46 +0530

    get posts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,20 +2,25 @@ const PostModel = require('../models/PostModel');
 
 exports.getPosts = (req, res) => {
     // res.send("Hello World from Node JS!");
-    res.json({
-        posts: [
-            {
-                title: "Node JS First Post",
-                author: "R Cube Dev",
-                posted_on: "2023-09-30 10:28:15"
-            },
-            {
-                title: "Node JS Second Post",
-                author: "Kanhu",
-                posted_on: "2023-09-26 02:04:45"
-            }
-        ]
-    });
+    
+    // res.json({
+    //     posts: [
+    //         {
+    //             title: "Node JS First Post",
+    //             author: "R Cube Dev",
+    //             posted_on: "2023-09-30 10:28:15"
+    //         },
+    //         {
+    //             title: "Node JS Second Post",
+    //             author: "Kanhu",
+    //             posted_on: "2023-09-26 02:04:45"
+    //         }
+    //     ]
+    // });
+
+    const posts = PostModel.find().then(posts => {
+        res.json({ posts });
+    }).catch(err => console.log(err));
 }
 
 exports.createPost = (req, res) => {
